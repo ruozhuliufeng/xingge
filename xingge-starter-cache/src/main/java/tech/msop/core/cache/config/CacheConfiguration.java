f461b2ffe98f1b262b4b1f09acbd98af98f50f8c
ruozhuliufeng <18852971173@163.com>
2025-08-07 22:48:27 +0800

    添加行歌-cache缓存框架

---
+++ xingge-starter-cache/src/main/java/tech/msop/core/cache/config/CacheConfiguration.java
@@ -0,0 +1,14 @@
+package tech.msop.core.cache.config;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
+
+/**
+ * 缓存配置
+ *
+ * @author ruozhuliufeng
+ */
+@AutoConfiguration
+@EnableCaching
+public class CacheConfiguration {
+}
---
+++ xingge-starter-cache/src/main/java/tech/msop/core/cache/constant/CacheConstant.java
@@ -0,0 +1,22 @@
+package tech.msop.core.cache.constant;
+
+/**
+ * 缓存常量名
+ *
+ * @author ruozhuliufeng
+ */
+public interface CacheConstant {
+    String BIZ_CACHE = "xg:biz";
+    String MENU_CACHE = "xg:menu";
+    String USER_CACHE = "xg:user";
+    String DICT_CACHE = "xg:dict";
+    String FLOW_CACHE = "xg:flow";
+    String SYS_CACHE = "xg:sys";
+    String RESOURCE_CACHE = "xg:resource";
+    String PARAM_CACHE = "xg:param";
+    String DEFAULT_CACHE = "default:cache";
+    String RETRY_LIMIT_CACHE = "retry:limit:cache";
+    String HALF_HOUR = "half:hour";
+    String HOUR = "hour";
+    String ONE_DAY = "one:day";
+}
